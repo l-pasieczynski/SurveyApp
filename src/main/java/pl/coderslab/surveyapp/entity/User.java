ed4f066dce2858b6e34efa2645f0232a50fb92f1
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-08-03 20:27:09 +0200

    Spring Security

---
+++ src/main/java/pl/coderslab/surveyapp/entity/User.java
@@ -0,0 +1,37 @@
+package pl.coderslab.surveyapp.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+@Entity
+@Setter
+@Getter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    @Column(nullable = false, unique = true, length = 60)
+    private String username;
+    @Column(nullable = false)
+    @Size(min = 6)
+    private String password;
+    @Column(nullable = false)
+    @Email
+    private String email;
+    private int enabled;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
+
+    public User() {
+    }
+}
