e4f263064659fc9e03e0f8e627f838855998c748
Krzysiek <k.dyr89@gmail.com>
2020-08-06 21:10:02 +0200

    Front simple

--- src/main/java/pl/coderslab/surveyapp/survey/AnswerRepository.java
+++ src/main/java/pl/coderslab/surveyapp/survey/AnswerRepository.java
@@ -2,6 +2,7 @@ package pl.coderslab.surveyapp.survey;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.coderslab.surveyapp.entity.Answer;
 
 @Repository
 interface AnswerRepository extends JpaRepository<Answer, Long>
