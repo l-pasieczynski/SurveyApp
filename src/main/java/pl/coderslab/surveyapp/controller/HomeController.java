b5e3148961f13e092272fe54d99b54f0155bf1a1
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-08-13 01:43:05 +0200

    REST HomeController (WIP)

--- src/main/java/pl/coderslab/surveyapp/controller/HomeController.java
+++ src/main/java/pl/coderslab/surveyapp/controller/HomeController.java
@@ -1,14 +1,17 @@
 package pl.coderslab.surveyapp.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import pl.coderslab.surveyapp.survey.FreeSurvey;
 import pl.coderslab.surveyapp.survey.SurveyFacade;
 import pl.coderslab.surveyapp.user.User;
 import pl.coderslab.surveyapp.user.UserService;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/")
 class HomeController {
 
@@ -20,53 +23,53 @@ class HomeController {
         this.surveyFacade = surveyFacade;
     }
 
-    @GetMapping
-    public String home() {
-        return "index.html";
-    }
+    private ModelAndView modelAndView = new ModelAndView();
 
-    @GetMapping("register")
-    public String register(Model model) {
-        model.addAttribute("user", new User());
-        return "register";
-    }
-
-    @PostMapping("")
-    public String registerUser(@RequestBody User user) {
+    @PostMapping(value = "register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void registerUser(@RequestBody User user) {
         userService.saveUser(user);
-        return "redirect:login";
+    }
+
+    @PostMapping(value = "login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public User loginUser(@RequestBody User user){
+        userService.loginUser(user);
+        return user;
+    }
+
+    @PostMapping(value = "surveys/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void freeSurveyPost(@RequestBody FreeSurvey freeSurvey) {
+        surveyFacade.saveFreeSurvey(freeSurvey);
+    }
+
+    @GetMapping
+    public ModelAndView index () {
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 
     @GetMapping("about")
-    public String about() {
-        return "about";
+    public ModelAndView about() {
+        modelAndView.setViewName("about");
+        return modelAndView;
     }
 
     @GetMapping("contact")
-    public String contact() {
-        return "contact";
+    public ModelAndView contact() {
+        modelAndView.setViewName("contact");
+        return modelAndView;
     }
 
-
     @GetMapping("surveys")
-    public String surveys(Model model) {
-        model.addAttribute("surveys", surveyFacade.findAllFreeSurveys());
-        return "surveys";
+    public ModelAndView surveys(Model model) {
+        List<FreeSurvey> surveys = surveyFacade.findAllFreeSurveys();
+        model.addAttribute("surveys", surveys);
+        modelAndView.setViewName("surveys");
+        return modelAndView;
     }
 
     @GetMapping("surveys/{id}")
     public String freeSurvey(@PathVariable("id") Long id, Model model) {
         model.addAttribute("surveys", surveyFacade.findFreeSurveyById(id));
         return "surveys/${id}";
-    }
-
-    @PostMapping("surveys/{id}")
-    public String freeSurveyPost(@RequestBody FreeSurvey freeSurvey) {
-        surveyFacade.saveFreeSurvey(freeSurvey);
-        return "surveys";
-    }
-    @GetMapping("survey")
-    public String survey(){
-        return "survey/survey02.html";
     }
 }
---
+++ src/main/resources/static/js/register.js
@@ -0,0 +1,29 @@
+$(function () {
+    let signUpBtn = $("#signUp");
+    signUpBtn.on("submit", sendRegisterForm);
+
+    function sendRegisterForm() {
+
+        let user = {
+            name: this.elements.name.value,
+            email: this.elements.email.value,
+            password: this.elements.password.value,
+            gender: this.elements.gender.value,
+        };
+
+        $.ajax({
+            url: "http://localhost:8080/register",
+            type: "POST",
+            data: JSON.stringify(user),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+        }).done(function () {
+        }).fail(function (xhr, status, err) {
+            console.log(xhr, status, err);
+        });
+
+        return false;
+
+    }
+
+});
