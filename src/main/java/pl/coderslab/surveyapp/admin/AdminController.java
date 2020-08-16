d2a8398803b1731b0acbff2ce6827eeb36b3e550
lukasz-pasieczynski <l.pasieczynski@gmail.com>
2020-08-16 18:04:03 +0200

    Admin Controller

--- src/main/java/pl/coderslab/surveyapp/admin/AdminController.java
+++ src/main/java/pl/coderslab/surveyapp/admin/AdminController.java
@@ -1,15 +1,23 @@
 package pl.coderslab.surveyapp.admin;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.surveyapp.survey.SurveyFacade;
 import pl.coderslab.surveyapp.user.UserService;
 
 @Controller
+@RequestMapping("/app/admin")
 public class AdminController {
 
     private final AdminService adminService;
 
     public AdminController(AdminService adminService) {
         this.adminService = adminService;
+    }
+
+    @GetMapping()
+    public String admin(){
+        return "admin";
     }
 }
